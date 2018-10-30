40d60ed0f0ad0a51cda4a2df58a376a1c50c20d2
Kevin Kipp <kevin.kipp@gmail.com>
2018-10-30 00:51:37 -0500

    First pass at first presentation

---
+++ theme.js
@@ -0,0 +1,32 @@
+// example theme.js
+import { dark } from "mdx-deck/themes";
+import codeTheme from "react-syntax-highlighter/styles/prism/twilight";
+import prismJs from "react-syntax-highlighter/languages/prism/javascript";
+import prismPython from "react-syntax-highlighter/languages/prism/python";
+
+export default {
+  // extends the default theme
+  ...dark,
+  // add a custom font
+  font: "monospace",
+  // custom colors
+  colors: {
+    text: "#dedede",
+    background: "#131313",
+    link: "#0ff"
+  },
+  blockquote: {
+    padding: "0 2em",
+    border: "0.3em solid rgb(84, 84, 84)",
+    borderRadius: ".5em",
+    fontSize: "1.1em",
+    fontWeight: "normal"
+  },
+  prism: {
+    style: codeTheme,
+    languages: {
+      javascript: prismJs,
+      python: prismPython
+    }
+  }
+};
